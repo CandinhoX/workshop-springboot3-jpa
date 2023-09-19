db1428c5efe54eaf3d044fe1d67d354b7b34ea62
CandinhoX <can_dinho7@hotmail.como>
2023-09-19 17:16:31 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
+++ src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
@@ -1,7 +1,7 @@
 package com.educandoweb.course.entities.enums;
 
 public enum OrderStatus {
-	
+
 	WAITING_PAYMENT(1),
 	PAID(2),
 	SHIPPED(3),
@@ -19,12 +19,11 @@ public enum OrderStatus {
 	}
 	
 	public static OrderStatus valueOf(int code) {
-		for(OrderStatus value : OrderStatus.values()) {
-			if(value.getCode() == code) {
-				
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
 			}
 		}
-		throw new IllegalAccessError("Invalid OrderStatus code");
+		throw new IllegalArgumentException("Invalid OrderStatus code");
 	}
-
 }
